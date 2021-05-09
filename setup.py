d5bb84b6814d26862db41f6198cc5db55ad2f9b4
Ian Good <ian@icgood.net>
2021-05-09 07:29:22 -0400

    Set version 0.3.3

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ with open('LICENSE.md') as f:
     license = f.read()
 
 setup(name='swim-protocol',
-      version='0.3.2',
+      version='0.3.3',
       author='Ian Good',
       author_email='ian@icgood.net',
       description='SWIM protocol implementation for exchanging cluster '
